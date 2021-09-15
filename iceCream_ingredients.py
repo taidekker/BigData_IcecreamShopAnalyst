506a40b7e9fa1045300c1c1ff9c846a1f0adb06d
taidekker <37642700+taidekker@users.noreply.github.com>
2021-09-15 14:54:25 -0700

    Update iceCream_ingredients.py

--- iceCream_ingredients.py
+++ iceCream_ingredients.py
@@ -1,5 +1,9 @@
 #Napapis Dekker
+"""
+The file Ingredients.csv contains the ice cream flavors and their ingredients for the store.
 
+Flavor codes are a linear model of the ingredients.
+"""
 import pandas as pd
 import statsmodels.api as sm
 import numpy as np
@@ -20,17 +24,17 @@ ing3 = data['Ingredient 3']
 ing4 = data['Ingredient 4']
 ing5 = data['Ingredient 5']
 ing6 = data['Ingredient 6']
-newFlavor = [47,31,48,18,49,43]
+newFlavor = [47,31,48,18,49,43] #Use the data to calculate the flavor code for a new flavor with this combination of ingredients:[47, 31, 48, 18, 49, 43]
 
 df = pd.DataFrame({'ing1':ing1,'ing2':ing2,'ing3':ing3,'ing4':ing4,'ing5':ing5,'ing6':ing6})
 X = df
 Y = Flavor
 model = sm.OLS(Y,X)
-print(model.fit().summary())
+print(model.fit().summary()) 
 
+#Sales seem to also be a linear function of the ingredients. Use the data to predict the expected sales for this new flavor.
 new_flavor = model.fit().predict(newFlavor)
 print (round(float(new_flavor),2))
-
 
 df_sale = pd.DataFrame({'ing1':ing1,'ing2':ing2,'ing4':ing4,'ing6':ing6})
 
@@ -40,6 +44,7 @@ X = sm.add_constant(X)
 model = sm.OLS(Y,X)
 print(model.fit().summary())
 
+#Print the summary of the linear models and the predictions. Round the predictions to two decimal places.
 new_sales = [1,47,31,18,43] 
 expected_sales = model.fit().predict(new_sales)
 print (round(float(expected_sales),2))
